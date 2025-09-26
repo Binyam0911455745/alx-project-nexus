f120c555426096e909532e4581231af1427309ce
binyam <binyamwork@gmail.com>
2025-09-26 15:10:56 +0300

    f' committing the changes for the case of deployment

--- nexus_project/settings.py
+++ nexus_project/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@kt#b%d)zg%3s5jz$fsd1c8ux=*w3b6w(jinh3$0t3ah8iz!wy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost'] # Add these for local dev, especially if you access via 127.0.0.1
 
